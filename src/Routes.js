d906224489f939c4ec57282676bf182e1a4839c4
Neel <nmakadiya1@gmail.com>
2021-03-06 18:27:56 +0530

    Added basic collabrative text editor

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Router } from '@reach/router';
 import Home from './Pages/Home/Home';
 import Profile from './Pages/Profile/Profile';
 import AddProfile from './Pages/AddProfile/AddProfile';
+import CreateNewBook from './Pages/CreateNewBook'
 
 
 export default function Routes() {
@@ -13,6 +14,7 @@ export default function Routes() {
         <Profile path="/:username" />
         <AddProfile path="/addprofile" />
         <Home path="*" />
+        <CreateNewBook path="/edit/:roomID"/>
       </Router>
     )
 }
