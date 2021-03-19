1f9a2eb7a503db0ed40c87ae74eb036e798f5601
yagnik <yagniksakhiya21@gmail.com>
2021-03-19 18:40:03 +0530

    m

--- src/Pages/CreateNewBook/index.js
+++ src/Pages/CreateNewBook/index.js
@@ -1,6 +1,6 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { v1 as uuid } from "uuid";
-import { CookiesProvider, Cookies,useCookies } from 'react-cookie';
+import { CookiesProvider, Cookies, useCookies } from 'react-cookie';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import * as Y from 'yjs'
@@ -20,15 +20,15 @@ export default function Room(props) {
     const ydoc = new Y.Doc()
 
     const cookies = new Cookies();
-    const userCookie=cookies.get('userCookie');
+    const userCookie = cookies.get('userCookie');
 
-    const username=userCookie.name;
+    const username = userCookie.name;
     const email = userCookie.email;
 
-    const provider = new WebsocketProvider('wss://demos.yjs.dev',`${roomID}`, ydoc)
+    const provider = new WebsocketProvider('wss://demos.yjs.dev', `${roomID}`, ydoc)
     const type = ydoc.getText(`${roomID}`);
 
-    useEffect(()=>{
+    useEffect(() => {
         const EditorContainer = document.getElementById("editor");
 
         var toolbarOptions = [
@@ -36,37 +36,37 @@ export default function Room(props) {
             [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
             ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
             [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-        
-            ['blockquote', 'link','image','video','code-block'],
+
+            ['blockquote', 'link', 'image', 'video', 'code-block'],
             ['formula'],
-        
+
             [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-            [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+            [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
             [{ 'direction': 'rtl' }],                         // text direction
             [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-              [{ 'align': [] }],
-              [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-            ['clean']  
-          ];
+            [{ 'align': [] }],
+            [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+            ['clean']
+        ];
 
         var editor = new Quill(EditorContainer, {
             modules: {
-            cursors : true,
-            toolbar:toolbarOptions,
-            history: {
-                userOnly: true
-            }
+                cursors: true,
+                toolbar: toolbarOptions,
+                history: {
+                    userOnly: true
+                }
             },
             placeholder: 'Start Writing...',
             theme: 'snow' // or 'bubble'
-        })   
+        })
 
-     //   editor.enable(false);
+        //   editor.enable(false);
 
         const binding = new QuillBinding(type, editor, provider.awareness)
         //console.log(provider.awareness);
-        if(!username)username='Anonymous';
+        if (!username) username = 'Anonymous';
         provider.awareness.setLocalStateField('user', {
             name: `${username}`,
             color: 'blue'
@@ -76,29 +76,30 @@ export default function Room(props) {
         window.example = { provider, ydoc, type, binding, Y }
     });
 
-   
-    
-   
-   
+
+
+
+
     return (
-            <div style={{display:"flex"}}>
-                <Grid
+        <div style={{ display: "flex" }}>
+            <Grid
                 container
                 direction="row"
                 justify="center"
                 alignItems="center"
                 spacing={1}
-                style={{height:100+'%' , display:"flex"}}
-                >
-                    <Grid item xs={12}>
-                        
-                    </Grid>
-                    <Grid item xs={10} style={{height:100+'%'}}>
-                        <Card>
-                            <div style={{ boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.19)"}} id="editor"></div>
-                        </Card>
-                    </Grid>
+                style={{ height: 100 + '%', display: "flex" }}
+            >
+                <Grid item xs={12}>
+
                 </Grid>
-            </div>
+                <Grid item xs={10} style={{ height: 100 + '%' }}>
+                    <Card>
+                        <div style={{ boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.19)" }} id="editor"></div>
+                    </Card>
+                </Grid>
+            </Grid>
+            {/* checking */}
+        </div>
     )
 }
