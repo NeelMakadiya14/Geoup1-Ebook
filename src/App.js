1cb9ea73330c795ca49dc4c32c0868dd8fba3028
Neel <nmakadiya1@gmail.com>
2021-03-19 17:18:24 +0530

    Resolve Some Bugs and Modified Google signIn rendering

--- src/App.js
+++ src/App.js
@@ -1,109 +1,11 @@
-import React, { Suspense, lazy, useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import Loader from "./components/Loader";
-import Home from "./Pages/Home/Home";
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Button from '@material-ui/core/Button';
-import { CookiesProvider, Cookies,useCookies } from 'react-cookie';
-import GoogleLogin from 'react-google-login';
-import Switch from "@material-ui/core/Switch";
-import CustomThemeProvider from './components/CustomThemeProvider';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Routes from './Routes';
-require('dotenv').config();
+import React from "react";
+import Routes from "./Routes";
+require("dotenv").config();
 
-const cookies = new Cookies();
-
-export default function App(){
-
-  const client_id=process.env.REACT_APP_CLIENT_ID ;
-  const [cookie, setCookie] = useCookies(['']);
-  const userCookie = cookies.get('userCookie');
-  const [user, setUser] = useState(false);
-
-  console.log(userCookie);
-
-  const responseGoogle = (response)=>{
-    console.log("Success");
-    let authCookie={
-      email:response.profileObj.email,
-      name:response.profileObj.name,
-      GID:response.googleId
-    }
-    console.log(authCookie);
-    setCookie('userCookie',authCookie);
-    setUser(true);
-  }
-  
-  const fail = (res)=>{
-    console.log("Failed ",res);
-  }
-  
-  useEffect(() => {
-    if (userCookie !== undefined) setUser(true);
-  });
-
-
-
-  function rand() {
-    return Math.round(Math.random() * 20) - 10;
-  }
-  
-  function getModalStyle() {
-    const top = 50 + rand();
-    const left = 50 + rand();
-  
-    return {
-      top: `${top}%`,
-      left: `${left}%`,
-      transform: `translate(-${top}%, -${left}%)`,
-    };
-  }
-  
-  const useStyles = makeStyles((theme) => ({
-    paper: {
-      position: 'absolute',
-      width: 400,
-      backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
-    },
-  }));
-
-
-
-  const classes = useStyles();
-  const [modalStyle] = React.useState(getModalStyle);
-  console.log(client_id);
-  const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <Button variant="contained" color="primary" onClick={()=>setUser(true)}>
-        Coninue as Guest
-      </Button>
-      <h2>Or</h2>
-      <GoogleLogin
-        clientId={client_id}
-        onSuccess={responseGoogle}
-        onFailure={fail}
-      >
-        <strong>Sign in with Google</strong>
-      </GoogleLogin>
+export default function App() {
+  return (
+    <div>
+      <Routes />
     </div>
   );
-
-  return (
-            <div>
-              <Routes/>
-              <Modal
-                open={!user}
-                onClose={()=>{setUser(false)}}
-              >
-                {body}
-              </Modal>
-            </div>
-      
-  );
-};
+}
--- src/components/MyAppBar.js
+++ src/components/MyAppBar.js
@@ -1,243 +1,224 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import SwitchUI from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import { CustomThemeContext } from './CustomThemeProvider';
-import { Link } from '@reach/router';
+import React, { useContext, useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import SwitchUI from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { CustomThemeContext } from "./CustomThemeProvider";
+import { Link } from "@reach/router";
 import { v1 as uuid } from "uuid";
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import IconButton from '@material-ui/core/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import { CookiesProvider, Cookies,useCookies } from 'react-cookie';
-import axios from 'axios';
-import queryString from 'query-string';
-import Button from '@material-ui/core/Button';
-import GoogleLogin from 'react-google-login';
-import { GoogleOutlined } from '@ant-design/icons';
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import IconButton from "@material-ui/core/IconButton";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import { CookiesProvider, Cookies, useCookies } from "react-cookie";
+import axios from "axios";
+import queryString from "query-string";
+import Button from "@material-ui/core/Button";
+import GoogleLogin from "react-google-login";
+import { GoogleOutlined } from "@ant-design/icons";
 import Navbar from "react-bootstrap/Navbar";
-import {NavDropdown,Nav} from "react-bootstrap";
-import Font, { Text } from 'react-font'
-
-
+import { NavDropdown, Nav } from "react-bootstrap";
+import Font, { Text } from "react-font";
 
 const useStyles = makeStyles((theme) => ({
-    appBar: {
-      zIndex: theme.zIndex.drawer + 1,
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }))
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-export default function MyAppBar() {
+export default function MyAppBar(props) {
+  const classes = useStyles();
 
-    const classes = useStyles();
+  const { currentTheme, setTheme } = useContext(CustomThemeContext);
+  const isDark = Boolean(currentTheme === "dark");
 
-    const { currentTheme, setTheme } = useContext(CustomThemeContext);
-    const isDark = Boolean(currentTheme === 'dark');
-
-    const handleThemeChange = (event) => {
-        const { checked } = event.target
-        if (checked) {
-          setTheme('dark')
-        } else {
-          setTheme('normal')
-        }
-      }
-  
-    console.log("From Home : ",currentTheme);
-
-    const id = uuid();
-
-    const [cookie, setCookie] = useCookies(['']);
-    const cookies = new Cookies();
-    const userCookie = cookies.get('userCookie');
-
-    //const email = userCookie.email;
-    const[isAuthor,setAuthor] = React.useState(true);
-    const [user, setUser] = useState(false);
-
-
-    const API_URL=process.env.REACT_APP_BACKEND_URL;
-    const client_id=process.env.REACT_APP_CLIENT_ID ;
-
-    if(userCookie!==undefined) {
-      const email = userCookie.email;
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      useEffect(()=>{
-        axios.get(`${API_URL}/checkauthor?` +queryString.stringify({ email  }))
-            .then((res) => {
-              if(res.data!==false){
-                setAuthor(true);
-              }
-            })
-            .catch((err) => console.log(err));
-      },[]);
+  const handleThemeChange = (event) => {
+    const { checked } = event.target;
+    if (checked) {
+      setTheme("dark");
+    } else {
+      setTheme("normal");
     }
+  };
 
+  console.log("From Home : ", currentTheme);
 
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-    const isMenuOpen = Boolean(anchorEl);
-    const handleProfileMenuOpen = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-    const handleMobileMenuClose = () => {
-      setMobileMoreAnchorEl(null);
-    };
-    const handleMenuClose = () => {
-      setAnchorEl(null);
-      handleMobileMenuClose();
-    };
+  const id = uuid();
 
-    const logout = (e) => {
-      //const { cookies } = this.props;
-      cookies.remove('userCookie');
-      window.location.href = '/';
-      // return false;
-  }
-    const menuId = 'primary-search-account-menu';
-    const renderMenu = (
+  const [cookie, setCookie] = useCookies([""]);
+  const cookies = new Cookies();
+  const userCookie = cookies.get("userCookie");
+  console.log("MYAPPBAR...");
+  const [isAuthor, setAuthor] = React.useState(true);
+
+  const API_URL = process.env.REACT_APP_BACKEND_URL;
+  const client_id = process.env.REACT_APP_CLIENT_ID;
+
+  useEffect(() => {
+    if (userCookie !== undefined) {
+      const email = userCookie.email;
+      axios
+        .get(`${API_URL}/checkauthor?` + queryString.stringify({ email }))
+        .then((res) => {
+          console.log(res.data);
+          res.data ? setAuthor(true) : setAuthor(false);
+          console.log("Value : ", isAuthor);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [props.render]);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const logout = (e) => {
+    //const { cookies } = this.props;
+    cookies.remove("userCookie");
+    window.location.href = "/";
+    // return false;
+  };
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
     <Menu
       anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={menuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
-      {isAuthor?<MenuItem component={Link} to="/Pages/Profile/Profile.js" onClick={handleMenuClose}>My Profile</MenuItem>:null}
-      
-      <MenuItem component={Link} to="/Pages/AddProfile/AddProfile.js" onClick={handleMenuClose}> Edit Profile  </MenuItem>
-      <MenuItem onClick={handleMenuClose} onClick={logout} >Logout</MenuItem>
+      {isAuthor ? (
+        <MenuItem
+          component={Link}
+          to={`profile/${userCookie ? userCookie.GID : ""}`}
+          onClick={handleMenuClose}
+        >
+          My Profile
+        </MenuItem>
+      ) : null}
+
+      <MenuItem component={Link} to="/editprofile" onClick={handleMenuClose}>
+        {" "}
+        Edit Profile{" "}
+      </MenuItem>
+      <MenuItem onClick={handleMenuClose} onClick={logout}>
+        Logout
+      </MenuItem>
     </Menu>
   );
 
-  const responseGoogle = (response)=>{
+  const responseGoogle = (response) => {
     console.log("Success");
-    let authCookie={
-      email:response.profileObj.email,
-      name:response.profileObj.name,
-      GID:response.googleId
-    }
+    let authCookie = {
+      email: response.profileObj.email,
+      name: response.profileObj.name,
+      GID: response.googleId,
+    };
     console.log(authCookie);
-    setCookie('userCookie',authCookie);
-    setUser(true);
-  }
-  
-  const fail = (res)=>{
-    console.log("Failed ",res);
-  }
+    setCookie("userCookie", authCookie);
+    props.setRender(!props.render);
+  };
 
+  const fail = (res) => {
+    console.log("Failed ", res);
+  };
 
-  function handleClick(){
-    <GoogleLogin
-        clientId={client_id}
-        onSuccess={responseGoogle}
-        onFailure={fail}
-      >
-       
-      </GoogleLogin>
-  }    
-
-
-  function login(){
-    <GoogleLogin
-        clientId={client_id}
-        buttonText=""
-        onSuccess={responseGoogle}
-        onFailure={fail}
-        size="medium"
-        //href="/"
-        render={renderProps => (
-            <GoogleOutlined onClick={renderProps.onClick} disabled={renderProps.disabled} style={{fontSize:'30px'}} />
-        )}
-      >
-      </GoogleLogin>
-      
-  }
-    
-    
-
-    return (
-      <div className={classes.grow}>
-
-
-        <AppBar position="fixed" className={classes.appBar}>
+  return (
+    <div className={classes.grow}>
+      <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
-
-          <Font family='Viga'>
-          <Button size="medium" href="/" style={{color:'white', fontSize:'20px', fontFamily:'Viga', fontStyle:'italic'}}>
-           eBook Maker
-          </Button>
+          <Font family="Viga">
+            <Button
+              size="medium"
+              href="/"
+              style={{
+                color: "white",
+                fontSize: "20px",
+                fontFamily: "Viga",
+                fontStyle: "italic",
+              }}
+            >
+              eBook
+            </Button>
           </Font>
-          
-           <FormControlLabel
-            style={{marginLeft:'5%'}}
+
+          <FormControlLabel
+            style={{ marginLeft: "5%" }}
             control={<SwitchUI checked={isDark} onChange={handleThemeChange} />}
             label="Theme"
-          /> 
+          />
 
+          <div style={{ marginLeft: "auto", marginRight: "20px" }}>
+            {userCookie == undefined ? null : isAuthor ? (
+              <Button
+                size="large"
+                href={`/edit/${id}`}
+                style={{ paddingRight: "20px", color: "white" }}
+              >
+                Create New Book
+              </Button>
+            ) : (
+              <Button
+                size="large"
+                href="/editprofile"
+                style={{ paddingRight: "20px", color: "white" }}
+              >
+                Create New Book
+              </Button>
+            )}
 
-          
-          <div style={{marginLeft: "auto", marginRight:"20px"}}>
-
-          {userCookie==undefined?
-          null :
-          isAuthor?
-            <Button size="large" href="/edit/${id}" style={{paddingRight:"20px", color:"white"}}>
-            Create New Book
-           </Button> :
-           <Button size="large" href="/addprofile" style={{paddingRight:"20px", color:"white"}}>
-           Create New Book
-          </Button> 
-          }
-          
-            
-        
-        
-        {userCookie===undefined?
-        <GoogleLogin
-        clientId={client_id}
-        buttonText=""
-        onSuccess={responseGoogle}
-        onFailure={fail}
-        size="medium"
-        //href="/"
-        render={renderProps => (
-            <GoogleOutlined onClick={renderProps.onClick} disabled={renderProps.disabled} style={{fontSize:'30px'}} />
-        )}
-      >
-        <Link to="/"> </Link>
-      </GoogleLogin>
-      
-      :
-          
-            <IconButton
+            {userCookie === undefined ? (
+              <GoogleLogin
+                clientId={client_id}
+                buttonText=""
+                onSuccess={responseGoogle}
+                onFailure={fail}
+                size="medium"
+                //href="/"
+                render={(renderProps) => (
+                  <GoogleOutlined
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                    style={{ fontSize: "30px" }}
+                  />
+                )}
+              >
+                <Link to="/"> </Link>
+              </GoogleLogin>
+            ) : (
+              <IconButton
                 edge="end"
                 aria-label="account of current user"
                 aria-controls={menuId}
                 aria-haspopup="true"
                 onClick={handleProfileMenuOpen}
                 color="inherit"
-                size='large' 
+                size="large"
               >
                 <AccountCircle />
               </IconButton>
-            
-           }
-
-           </div>
-
-          
+            )}
+          </div>
         </Toolbar>
       </AppBar>
       {renderMenu}
-      </div>
-    );
+    </div>
+  );
 }
