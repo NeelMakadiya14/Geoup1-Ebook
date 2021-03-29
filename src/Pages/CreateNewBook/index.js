2b4e009d78c3f8eaa60c5ea086783eb0f647280d
harshvora10101 <harshvora42719@gmail.com>
2021-03-29 12:29:14 +0530

    added Download pdf functionality

--- src/Pages/CreateNewBook/index.js
+++ src/Pages/CreateNewBook/index.js
@@ -1,6 +1,6 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { v1 as uuid } from "uuid";
-import { CookiesProvider, Cookies,useCookies } from 'react-cookie';
+import { CookiesProvider, Cookies, useCookies } from 'react-cookie';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import * as Y from 'yjs'
@@ -11,6 +11,10 @@ import QuillCursors from 'quill-cursors';
 import MyAppBar from '../../components/MyAppBar';
 import { MoneyOffRounded } from '@material-ui/icons';
 import Button from '@material-ui/core/Button';
+import { render } from 'react-dom';
+import { renderToString } from 'react-dom/server';
+//import Hello from './Hello';
+import jsPDF from 'jspdf';
 
 export default function Room(props) {
     const roomID = props.roomID;
@@ -20,53 +24,52 @@ export default function Room(props) {
     const ydoc = new Y.Doc()
 
     const cookies = new Cookies();
-    const userCookie=cookies.get('userCookie');
+    const userCookie = cookies.get('userCookie');
 
-    const username=userCookie.name;
+    const username = userCookie.name;
     const email = userCookie.email;
 
-    const provider = new WebsocketProvider('wss://demos.yjs.dev',`${roomID}`, ydoc)
+    const provider = new WebsocketProvider('wss://demos.yjs.dev', `${roomID}`, ydoc)
     const type = ydoc.getText(`${roomID}`);
-
-    useEffect(()=>{
+    var Harsh = document.getElementsByClassName("ql-editor");
+    useEffect(() => {
         const EditorContainer = document.getElementById("editor");
-
         var toolbarOptions = [
             [{ 'font': [] }],
             [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
             ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
             [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-        
-            ['blockquote', 'link','image','video','code-block'],
+
+            ['blockquote', 'link', 'image', 'video', 'code-block'],
             ['formula'],
-        
+
             [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-            [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+            [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
             [{ 'direction': 'rtl' }],                         // text direction
             [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-              [{ 'align': [] }],
-              [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-            ['clean']  
-          ];
+            [{ 'align': [] }],
+            [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+            ['clean']
+        ];
 
         var editor = new Quill(EditorContainer, {
             modules: {
-            cursors : true,
-            toolbar:toolbarOptions,
-            history: {
-                userOnly: true
-            }
+                cursors: true,
+                toolbar: toolbarOptions,
+                history: {
+                    userOnly: true
+                }
             },
             placeholder: 'Start Writing...',
-            theme: 'snow' // or 'bubble'
-        })   
+            theme: 'snow', // or 'bubble'
+            // onChange: { rteChange },
+            // value: { state.comments || '' },
 
-     //   editor.enable(false);
-
+        })
         const binding = new QuillBinding(type, editor, provider.awareness)
-        //console.log(provider.awareness);
-        if(!username)username='Anonymous';
+        console.log(provider.awareness);
+        if (!username) username = 'Anonymous';
         provider.awareness.setLocalStateField('user', {
             name: `${username}`,
             color: 'blue'
@@ -74,31 +77,42 @@ export default function Room(props) {
 
         // @ts-ignore
         window.example = { provider, ydoc, type, binding, Y }
-    });
+    }, []);
 
-   
-    
-   
-   
+
+
+    const print = () => {
+        console.log(Harsh);
+        //var string = renderToString(Harsh.item(0));
+        var pdf = new jsPDF();
+        pdf.fromHTML(Harsh.item(0));
+        //console.log(string);
+        pdf.save('pdf')
+    }
+
+
     return (
-            <div style={{display:"flex"}}>
+        <div>
+            <div style={{ display: "flex" }}>
                 <Grid
-                container
-                direction="row"
-                justify="center"
-                alignItems="center"
-                spacing={1}
-                style={{height:100+'%' , display:"flex"}}
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                    spacing={1}
+                    style={{ height: 200 + '%', display: "flex" }}
                 >
                     <Grid item xs={12}>
-                        
+
                     </Grid>
-                    <Grid item xs={10} style={{height:100+'%'}}>
+                    <Grid item xs={10} style={{ height: 100 + '%' }}>
                         <Card>
-                            <div style={{ boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.19)"}} id="editor"></div>
+                            <div style={{ boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.19)" }} id="editor"></div>
                         </Card>
                     </Grid>
                 </Grid>
             </div>
+            <button onClick={print}>Download</button>
+        </div>
     )
 }
