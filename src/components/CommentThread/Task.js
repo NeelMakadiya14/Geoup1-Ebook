49f722fc62f53dd3805a7655e222a8b91befc3d1
Neel Makadiya <45729507+NeelMakadiya14@users.noreply.github.com>
2021-04-11 16:12:22 +0530

    Revert "Revert "Comment Thread ""

---
+++ src/components/CommentThread/Task.js
@@ -0,0 +1,10 @@
+const Task = ({ task }) => {
+    return (
+        <div>
+            <h3>{task.name}{' '}</h3>
+            <p> {task.comm}</p>
+        </div>
+    )
+}
+
+export default Task
---
+++ src/components/CommentThread/Tasks.js
@@ -0,0 +1,13 @@
+import Task from './Task'
+
+const Tasks = ({ tasks }) => {
+    return (
+        <div className="First">
+            {tasks.map((task, index) => (
+                <Task key={index} task={task} />
+            ))}
+        </div>
+    )
+}
+
+export default Tasks
