93b008f65581825bc5f26b9715ba2e5ecd30402a
Jay30801 <76864215+Jay30801@users.noreply.github.com>
2021-04-24 16:05:35 +0530

    3rd

--- src/components/CommentThread/Comment.js
+++ src/components/CommentThread/Comment.js
@@ -1,40 +1,38 @@
 import { useState } from 'react'
 
-const AddTask = ({ onAdd, name, userId, username, props }) => {
-    //const [name, setName] = useState('')
-    const [comm, setComm] = useState('')
+const AddTask = ({ onAdd, usercookie, props }) => {
+  //const [name, setName] = useState('Jay')
+  const [comm, setComm] = useState('')
 
+  const onSubmit = (e) => {
+    e.preventDefault()
 
-    const onSubmit = (e) => {
-        e.preventDefault()
-
-        if (!comm) {
-            alert('Please add a comment')
-            return
-        }
-
-        onAdd({ comm, name, username, props.bookID })
-
-        //setName('')
-        setComm('')
+    if (!comm) {
+      alert('Please add a comment')
+      return
     }
+    console.log("Comment is ", comm, usercookie.name, props.bookID);
+    onAdd({ comment: comm, Gname: usercookie.name, GID: usercookie.GID, docID: props.bookID })
+    //  setName('')
+    setComm('')
+  }
 
-    return (
-        <form className='add-form' onSubmit={onSubmit}>
-            <div className='form-control'>
-            </div>
-            <div className='form-control'>
-                <label>Comments</label>
-                <input
-                    type='text'
-                    placeholder='Comment'
-                    value={comm}
-                    onChange={(e) => setComm(e.target.value)}
-                />
-            </div>
-            <input type='submit' value='Add Comment' className='btn btn-block' />
-        </form>
-    )
+  return (
+    <form className='add-form' onSubmit={onSubmit}>
+      <div className='form-control'>
+      </div>
+      <div className='form-control'>
+        <label>Comments</label>
+        <input
+          type='text'
+          placeholder='Add Comment'
+          value={comm}
+          onChange={(e) => setComm(e.target.value)}
+        />
+      </div>
+      <input type='submit' value='Add Comment' className='btn btn-block' />
+    </form>
+  )
 }
 
 export default AddTask
