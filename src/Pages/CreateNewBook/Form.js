73d976584419d86fa0ae24942fe8af5e73ce6d9e
yagnik <yagniksakhiya21@gmail.com>
2021-05-05 23:18:02 +0530

    done

--- src/Pages/CreateNewBook/Form.js
+++ src/Pages/CreateNewBook/Form.js
@@ -3,9 +3,9 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
+
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import LibraryBooks from '@material-ui/icons/LibraryBooks';
 import { useState } from 'react'
@@ -13,56 +13,49 @@ import axios from 'axios';
 import queryString from 'query-string';
 import { CookiesProvider, Cookies, useCookies } from 'react-cookie';
 import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import { Email } from '@material-ui/icons';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { genres } from '../../utils/Constant';
-
+import Box from '@material-ui/core/Box';
 
 require('dotenv').config();
 
+const CloudName = process.env.REACT_APP_CLOUD_NAME;
+const UploadPreset = process.env.REACT_APP_CLOUD_PRESET;
+
+const defaultProps = {
+    border: 1,
+    borderRadius: 5,
+    style: { width: '396px', height: '57px' , marginTop: '20px' },
+  };
+  
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
-
-
 
 export default function Form() {
     const classes = useStyles();
     const [bookname, setBookName] = useState('');
     const [description, setDescription] = useState('');
     const [genre, setGenre] = useState({});
-
-    const [err, setErr] = useState(false);
-
-
-
-    const onClick = () => {
-
-        if (!bookname.trim()) {
-            setErr(true);
-            return;
-        }
-        setErr(false);
+    const [options, setOptions] = useState();
+    const submit = async () => {
         const cookies = new Cookies();
         const userCookie = cookies.get('userCookie');
         const email = userCookie.email;
@@ -70,99 +63,104 @@ export default function Form() {
         console.log(userCookie.GID);
         let id = window.location.pathname;
         id = id.substring(6);
-        console.log(genre);
+        let secure;
+        await fetch(`https://api.Cloudinary.com/v1_1/${CloudName}/image/upload`, options)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res.secure_url);
+                secure = res.secure_url;
+            })
+            .catch(err => console.log(err));
+
+        console.log(secure);
         const obj = {
             title: bookname,
             description: description,
             genres: genre,
-            docID: id
+            docID: id,
+            imageUrl: secure
         }
+
         axios.post(`${API_URL}/startwriting?` + queryString.stringify({ email }), obj)
             .then((res) => {
                 window.location.reload();
             })
+
+    }
+
+    const onClick = () => {
+        if (formRef.current.reportValidity()) submit();
     }
 
 
+    const handleUploadClick = (event) => {
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LibraryBooks />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Create New Book
-                </Typography>
-                <form className={classes.form} noValidate>
-                    {err && bookname.length === 0 ?
-                        <TextField error
-                            className="color"
-                            variant="outlined"
-                            margin="normal"
-                            id="outlined-error"
-                            required
-                            fullWidth
-                            label="Book Name"
-                            id="standard-error-helper-text"
-                            helperText="Enter book name"
-                            onChange={event => setBookName(event.target.value)}
-                        />
-                        :
-                        <TextField
-                            className="color"
-                            variant="outlined"
-                            margin="normal"
-                            id="outlined-error"
-                            required
-                            fullWidth
-                            label="Book Name"
-                            onChange={event => setBookName(event.target.value)}
-                        />
-                    }
+        const formData = new FormData();
+        formData.append('file', event.target.files[0]);
+        // replace this with your upload preset name
+        formData.append('upload_preset', UploadPreset);
+        setOptions({
+            method: 'POST',
+            body: formData,
+        });
 
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        fullWidth
-                        label="Description"
-                        onChange={event => setDescription(event.target.value)}
-                    />
-
-
-                    <div className={classes.root}>
-                        <Autocomplete
-                            multiple
-                            id="tags-outlined"
-                            options={genres}
-                            getOptionLabel={(option) => option}
-                            filterSelectedOptions
-                            renderInput={(params) => (
-                                <TextField
-                                    {...params}
-                                    variant="outlined"
-                                    label="Genres of Book"
-                                    placeholder="new genre"
-                                />
-                            )}
-                            onChange={(event, value) => setGenre(value)}
-                        />
-                    </div>
-
-                    <Button
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                        onClick={onClick}
-                    >
-                        Create Book
-                    </Button>
-
-                </form>
-            </div>
-
-        </Container>
-    );
+    }
+    
+  const formRef = React.useRef();
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LibraryBooks />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Create New Book
+        </Typography>
+        <form className={classes.form} ref={formRef}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            margin="normal"
+            label="Book Name"
+            onChange={(event) => setBookName(event.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            fullWidth
+            label="Description"
+            margin="normal"
+            onChange={(event) => setDescription(event.target.value)}
+          />
+          <Autocomplete
+            multiple
+            options={genres}
+            style={{ marginTop: "15px" }}
+            getOptionLabel={(option) => option}
+            filterSelectedOptions
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                required={genre.length === undefined || genre.length === 0}
+                variant="outlined"
+                label="Genres of Book"
+                placeholder="new genre"
+              />
+            )}
+            onChange={(event, value) => setGenre(value)}
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={onClick}
+          >
+            Create Book
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
 }
--- src/Pages/CreateNewBook/editor.js
+++ src/Pages/CreateNewBook/editor.js
@@ -1,3 +1,5 @@
+
+
 import React, { useState, useEffect } from "react";
 import { v1 as uuid } from "uuid";
 import { CookiesProvider, Cookies, useCookies } from "react-cookie";
@@ -10,36 +12,51 @@ import Quill from "quill";
 import QuillCursors from "quill-cursors";
 import MyAppBar from "../../components/MyAppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import { MoneyOffRounded } from "@material-ui/icons";
 import Button from "@material-ui/core/Button";
-import { render } from "react-dom";
-import { renderToString } from "react-dom/server";
 import queryString from "query-string";
-//import Hello from './Hello';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+
 import axios from "axios";
+
 import jsPDF from "jspdf";
+import html2pdf from "html2pdf.js";
+
+require("dotenv").config();
+
+const API_URL = process.env.REACT_APP_BACKEND_URL;
+
+const cookies = new Cookies();
+const userCookie = cookies.get("userCookie");
+
+const username = userCookie.name;
+const email = userCookie.email;
+
+let docID = window.location.pathname;
+docID = docID.substring(6);
+
+
 
 export default function Room(props) {
   const roomID = props.roomID;
-  const API_URL = process.env.REACT_APP_BACKEND_URL;
   Quill.register("modules/cursors", QuillCursors);
-
+  //const ImageResize = require('quill-image-resize-module').default
+  //Quill.register('modules/imageResize', ImageResize);
   const ydoc = new Y.Doc();
-
-  const cookies = new Cookies();
-  const userCookie = cookies.get("userCookie");
-
-  const username = userCookie.name;
-  const email = userCookie.email;
-
   const provider = new WebsocketProvider(
     "wss://demos.yjs.dev",
     `${roomID}`,
     ydoc
   );
   const type = ydoc.getText(`${roomID}`);
-  var Harsh = document.getElementsByClassName("ql-editor");
-  useEffect(() => {
+  var htmlContent = document.getElementsByClassName("ql-editor");
+
+  const [chapters, setChapters] = useState([]);
+
+
+  useEffect(async () => {
     const EditorContainer = document.getElementById("editor");
     var toolbarOptions = [
       [{ font: [] }],
@@ -59,7 +76,6 @@ export default function Room(props) {
       [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
       ["clean"],
     ];
-
     var editor = new Quill(EditorContainer, {
       modules: {
         cursors: true,
@@ -72,6 +88,7 @@ export default function Room(props) {
       theme: "snow", // or 'bubble'
       // onChange: { rteChange },
       // value: { state.comments || '' },
+
     });
     const binding = new QuillBinding(type, editor, provider.awareness);
     console.log(provider.awareness);
@@ -83,62 +100,161 @@ export default function Room(props) {
 
     // @ts-ignore
     window.example = { provider, ydoc, type, binding, Y };
+
+    await axios
+      .get(`${API_URL}/bookbyid?` + queryString.stringify({ docID }))
+      .then((res) => setChapters(res.data.chapters)
+      );
+
   }, []);
+
+
 
   const OnSave = async () => {
     console.log(API_URL);
-    var pdf = new jsPDF();
-    pdf.fromHTML(Harsh.item(0), 0, 0, { width: 100 }, function () {
-      var blob = pdf.output("blob");
-      const data = new FormData();
-      data.append("book", blob);
+    // var pdf = new jsPDF();
+    // html2pdf(pdf);
+    var final = htmlContent.item(0);
+    // var pdf = html2pdf().from(final);
+    html2pdf()
+      .from(final)
+      .outputPdf("blob")
+      .then((result) => {
+        var data = new FormData();
+        data.append("book", result);
 
-      var requestOptions = {
-        method: "POST",
-        body: data,
-        redirect: "follow",
-      };
+        var requestOptions = {
+          method: "POST",
+          body: data,
+          redirect: "follow",
+        };
 
-      fetch(
-        `${API_URL}/uploadbook?` + queryString.stringify({ docID: roomID }),
-        requestOptions
-      )
-        .then((response) => response.text())
-        .then((result) => console.log(result))
-        .catch((error) => console.log("error", error));
+        fetch(
+          `${API_URL}/uploadbook?` + queryString.stringify({ docID: roomID }),
+          requestOptions
+        )
+          .then((response) => response.text())
+          .then((result) => console.log(result))
+          .catch((error) => console.log("error", error));
+      });
 
-      pdf.save("pdf");
-    });
+    // pdf.fromHTML(htmlContent.item(0), 0, 0, { width: 100 }, function () {
+    //   var blob = pdf.output("blob");
+    //   var data = new FormData();
+    //   data.append("book", blob);
+
+    //   var requestOptions = {
+    //     method: "POST",
+    //     body: data,
+    //     redirect: "follow",
+    //   };
+
+    //   fetch(
+    //     `http://localhost:5001/uploadbook?` +
+    //       queryString.stringify({ docID: roomID }),
+    //     requestOptions
+    //   )
+    //     .then((response) => response.text())
+    //     .then((result) => console.log(result))
+    //     .catch((error) => console.log("error", error));
+    // });
+    // pdf.save("pdf");
   };
+
+  let id = window.location.pathname;
+  id = id.substring(6);
+
+  const AddCollabator = () => {
+    let foo = prompt('Type here email of collabator');
+    if (foo !== null) {
+      const obj = {
+        email: foo
+      }
+
+      axios.post(`${API_URL}/addeditor?` + queryString.stringify({ docID: id }), obj)
+        .then((res) => console.log(res))
+    }
+  }
+
+  const AddChapter = async () => {
+    let foo = prompt('Add new chapter');
+    if (foo !== null) {
+      const obj = {
+        cname: foo
+      }
+
+      await axios.post(`${API_URL}/addchapter?` + queryString.stringify({ docID: id }), obj)
+        .then((res) => console.log(res))
+
+      setChapters([...chapters,foo]);
+      console.log(chapters);
+    }
+  }
 
   return (
     <div>
       <MyAppBar />
       <Toolbar />
       <div style={{ display: "flex" }}>
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-          spacing={1}
-          style={{ height: 200 + "%", display: "flex" }}
-        >
-          <Grid item xs={12}></Grid>
-          <Grid item xs={10} style={{ height: 100 + "%" }}>
-            <Card>
-              <div
-                style={{
-                  boxShadow:
-                    "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.19)",
-                }}
-                id="editor"
-              ></div>
-            </Card>
-          </Grid>
+
+
+        <Grid item xs={2} style={{ height: 100 + "%" }}>
+          <Card>
+            <div>
+              <h1> Chapters </h1>
+              <List>
+              {chapters.map ( (chapter) => (
+                <ListItem>
+                  <ListItemText
+                    primary={chapter}
+                  />
+                </ListItem>
+              )) 
+              }
+              </List>
+            </div>
+          </Card>
         </Grid>
+
+        <Grid item xs={10} style={{ height: 100 + "%" }}>
+          <Card>
+            <div
+              style={{
+                boxShadow:
+                  // "0 4px 8px 0 rgba(100, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 100, 0, 0.19)",
+                  "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.19)",
+              }}
+              id="editor"
+            ></div>
+          </Card>
+        </Grid>
+
+
       </div>
-      <button onClick={OnSave}>Save</button>
+
+      <div
+        style={{
+          padding: "15px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+
+
+        <Button variant="contained" color="primary" onClick={AddCollabator}>
+          Add collabator
+        </Button>
+
+        <Button variant="contained" color="primary" style={{ marginLeft: 5 }} onClick={AddChapter}>
+          Add Chapter
+        </Button>
+
+        <Button variant="contained" color="primary" style={{ marginLeft: 5 }} onClick={OnSave}>
+          Submit
+        </Button>
+
+      </div>
     </div>
   );
 }
